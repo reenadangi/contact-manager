76a72b1b1ed8ebc60dde912533d0cae4103d21df
reenadangi <reena.dangi@gmail.com>
2020-05-27 14:53:27 -0500

    Contact Keeper- contactItem

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -5,8 +5,24 @@ export const ContactItem = ({contact}) => {
     return (
         <div className='card bg-light'>
             <h3 className="text-primary text-left">
-    {name}{' '} <span className={'badge '+(type==='professional'?'badge-success':'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+             {name}{' '} 
+                <span style={{float:'right'}} 
+                className={'badge '+(type==='professional'?'badge-success':'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}
+                </span>
             </h3>
+            <ul className="list">
+                {/* if email and phone is there */}
+                {email && (<li>
+                <i className="fas fa-envelope-open"> {email}</i>
+                </li>)}
+                {phone && (<li>
+                <i className="fas fa-phone"> {phone}</i>
+                </li>)}
+            </ul>
+            <p>
+                <button className="btn btn-dark btn-sm">Edit</button>
+                <button className="btn btn-danger btn-sm">Delete</button>
+            </p>
             
         </div>
     )
