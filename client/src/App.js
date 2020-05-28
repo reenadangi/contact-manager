b5956fc144b872a209778b225e631eed05ea4f2f
reenadangi <reena.dangi@gmail.com>
2020-05-28 16:00:38 -0500

    Auth Register-login

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React, { Fragment } from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+
 
 import NavBar from './components/layout/Navbar';
 import ContactState from './context/contact/ContactState';
@@ -21,7 +25,9 @@ const App=()=> {
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/about" component={About}/>
-
+          <Route exact path="/register" component={Register}/>
+          
+          <Route exact path="/login" component={Login}/>
         </Switch>
       </div>
       </Fragment>
